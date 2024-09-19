9a83aaf708c26462c8e564e12e3f8810b560927c
HouckDev <houckdeveloper@gmail.com>
2024-09-19 11:15:44 -0500

    Implement BFRegister

--- src/main/java/edu/grinnell/csc207/util/BFRegister.java
+++ src/main/java/edu/grinnell/csc207/util/BFRegister.java
@@ -3,22 +3,22 @@ package edu.grinnell.csc207.util;
 import java.util.HashMap;
 
 public class BFRegister {
-  public static HashMap<Character,BigFraction> register = new HashMap<Character,BigFraction>();
+  HashMap<Character,BigFraction> register = new HashMap<Character,BigFraction>();
  
   /**
    * @param register
    * @param val
    */
-  public void store(char register, BigFraction val) {
-
+  public void store(char key, BigFraction val) {
+    register.put(key,val);
   } // store
 
   /**
    * @param register
    * @return
    */
-  public BigFraction get(char register) {
-    return null;
+  public BigFraction get(char key) {
+    return register.get(key);
 
   } // get
 } // BFRegister

4b0df0972b30926713d30e12029c6bb512ab4dc8
HouckDev <houckdeveloper@gmail.com>
2024-09-19 11:10:11 -0500

    BFCalculator skeleton

--- src/main/java/edu/grinnell/csc207/util/BFCalculator.java
+++ src/main/java/edu/grinnell/csc207/util/BFCalculator.java
@@ -1,5 +1,27 @@
 package edu.grinnell.csc207.util;
 
 public class BFCalculator {
-  
-}
+  public BigFraction get() {
+    return null;
+  } // get
+
+  public void add(BigFraction val) {
+
+  } // add
+
+  public void subtract(BigFraction val) {
+
+  } // subtract
+
+  public void multiply(BigFraction val) {
+
+  } // multiply
+
+  public void divide(BigFraction val) {
+     
+  } // divide
+
+  public void clear() {
+
+  } // clear
+} // BFCalculator
